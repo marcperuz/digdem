4ddfe587213598f0db60d15996076572767bc0da
Peruzzetto  Marc <m.peruzzetto@brgm.fr>
2024-01-04 13:27:47 +0100

    style(linting): Implement ruff formatting suggestions
    Implement ruff formatting suggestions to abide by best practices

--- src/digdem/plot.py
+++ src/digdem/plot.py
@@ -10,13 +10,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
-BOLD_CONTOURS_INTV = [0.1, 0.2, 0.5, 1, 2., 5, 10, 20, 50, 100, 200, 500, 1000]
+BOLD_CONTOURS_INTV = [0.1, 0.2, 0.5, 1, 2.0, 5, 10, 20, 50, 100, 200, 500, 1000]
 NB_THIN_CONTOURS = 10
 NB_BOLD_CONTOURS = 3
 
-def get_contour_intervals(zmin, zmax, nb_bold_contours=None,
-                          nb_thin_contours=None):
 
+def get_contour_intervals(zmin, zmax, nb_bold_contours=None, nb_thin_contours=None):
     if nb_thin_contours is None:
         nb_thin_contours = NB_THIN_CONTOURS
     if nb_bold_contours is None:
@@ -28,28 +27,46 @@ def get_contour_intervals(zmin, zmax, nb_bold_contours=None,
     bold_intv = BOLD_CONTOURS_INTV[i]
     if BOLD_CONTOURS_INTV[i] != BOLD_CONTOURS_INTV[0]:
         if bold_intv - intv > 0:
-            bold_intv = BOLD_CONTOURS_INTV[i-1]
+            bold_intv = BOLD_CONTOURS_INTV[i - 1]
 
     if nb_thin_contours is None:
         thin_intv = bold_intv / NB_THIN_CONTOURS
-        if (zmax - zmin)/bold_intv > 5:
-            thin_intv = thin_intv*2
+        if (zmax - zmin) / bold_intv > 5:
+            thin_intv = thin_intv * 2
     else:
         thin_intv = bold_intv / nb_thin_contours
 
     return bold_intv, thin_intv
 
 
-def plot_topo(z, x, y, contour_step=None, nlevels=None, level_min=None,
-              step_contour_bold='auto', contour_labels_properties=None,
-              label_contour=True, contour_label_effect=None,
-              axe=None,
-              vert_exag=1, fraction=1, ndv=-9999, uniform_grey=None,
-              contours_prop=None, contours_bold_prop=None,
-              figsize=None,
-              interpolation=None,
-              sea_level=0, sea_color=None, alpha=1, azdeg=315, altdeg=45,
-              zmin=None, zmax=None):
+def plot_topo(
+    z,
+    x,
+    y,
+    contour_step=None,
+    nlevels=None,
+    level_min=None,
+    step_contour_bold="auto",
+    contour_labels_properties=None,
+    label_contour=True,
+    contour_label_effect=None,
+    axe=None,
+    vert_exag=1,
+    fraction=1,
+    ndv=-9999,
+    uniform_grey=None,
+    contours_prop=None,
+    contours_bold_prop=None,
+    figsize=None,
+    interpolation=None,
+    sea_level=0,
+    sea_color=None,
+    alpha=1,
+    azdeg=315,
+    altdeg=45,
+    zmin=None,
+    zmax=None,
+):
     """
     Plot topography with hillshading.
 
@@ -105,21 +122,17 @@ def plot_topo(z, x, y, contour_step=None, nlevels=None, level_min=None,
     """
     dx = x[1] - x[0]
     dy = y[1] - y[0]
-    im_extent = [x[0]-dx/2,
-                 x[-1]+dx/2,
-                 y[0]-dy/2,
-                 y[-1]+dy/2]
+    im_extent = [x[0] - dx / 2, x[-1] + dx / 2, y[0] - dy / 2, y[-1] + dy / 2]
     ls = mcolors.LightSource(azdeg=azdeg, altdeg=altdeg)
 
     auto_bold_intv = None
 
     if nlevels is None and contour_step is None:
-        auto_bold_intv, contour_step = get_contour_intervals(np.nanmin(z),
-                                                             np.nanmax(z))
+        auto_bold_intv, contour_step = get_contour_intervals(np.nanmin(z), np.nanmax(z))
 
     if level_min is None:
         if contour_step is not None:
-            level_min = np.ceil(np.nanmin(z)/contour_step)*contour_step
+            level_min = np.ceil(np.nanmin(z) / contour_step) * contour_step
         else:
             level_min = np.nanmin(z)
     if contour_step is not None:
@@ -132,46 +145,52 @@ def plot_topo(z, x, y, contour_step=None, nlevels=None, level_min=None,
         axe = fig.gca()
     else:
         fig = axe.figure
-    axe.set_ylabel('Y (m)')
-    axe.set_xlabel('X (m)')
-    axe.set_aspect('equal')
+    axe.set_ylabel("Y (m)")
+    axe.set_xlabel("X (m)")
+    axe.set_aspect("equal")
 
     if uniform_grey is None:
-        shaded_topo = ls.hillshade(z,
-                                   vert_exag=vert_exag, dx=dx, dy=dy,
-                                   fraction=1)
+        shaded_topo = ls.hillshade(z, vert_exag=vert_exag, dx=dx, dy=dy, fraction=1)
     else:
-        shaded_topo = np.ones(z.shape)*uniform_grey
+        shaded_topo = np.ones(z.shape) * uniform_grey
     shaded_topo[z == ndv] = np.nan
-    axe.imshow(shaded_topo, cmap='gray', extent=im_extent,
-               interpolation=interpolation, alpha=alpha, vmin=0, vmax=1)
+    axe.imshow(
+        shaded_topo,
+        cmap="gray",
+        extent=im_extent,
+        interpolation=interpolation,
+        alpha=alpha,
+        vmin=0,
+        vmax=1,
+    )
 
     if contours_prop is None:
-        contours_prop = dict(alpha=0.5, colors='k',
-                             linewidths=0.5)
-    axe.contour(x, y, np.flip(z, axis=0), extent=im_extent,
-                levels=levels,
-                **contours_prop)
+        contours_prop = dict(alpha=0.5, colors="k", linewidths=0.5)
+    axe.contour(
+        x, y, np.flip(z, axis=0), extent=im_extent, levels=levels, **contours_prop
+    )
 
     if contours_bold_prop is None:
-        contours_bold_prop = dict(alpha=0.8, colors='k',
-                                  linewidths=0.8)
+        contours_bold_prop = dict(alpha=0.8, colors="k", linewidths=0.8)
 
-    if step_contour_bold == 'auto':
+    if step_contour_bold == "auto":
         if auto_bold_intv is None:
-            auto_bold_intv, _ = get_contour_intervals(np.nanmin(z),
-                                                      np.nanmax(z))
+            auto_bold_intv, _ = get_contour_intervals(np.nanmin(z), np.nanmax(z))
         step_contour_bold = auto_bold_intv
 
     if step_contour_bold > 0:
-        lmin = np.ceil(np.nanmin(z)/step_contour_bold)*step_contour_bold
+        lmin = np.ceil(np.nanmin(z) / step_contour_bold) * step_contour_bold
         if lmin < level_min:
             lmin = lmin + step_contour_bold
         levels = np.arange(lmin, np.nanmax(z), step_contour_bold)
-        cs = axe.contour(x, y, np.flip(z, axis=0),
-                         extent=im_extent,
-                         levels=levels,
-                         **contours_bold_prop)
+        cs = axe.contour(
+            x,
+            y,
+            np.flip(z, axis=0),
+            extent=im_extent,
+            levels=levels,
+            **contours_bold_prop,
+        )
         if label_contour:
             if contour_labels_properties is None:
                 contour_labels_properties = {}
@@ -181,8 +200,14 @@ def plot_topo(z, x, y, contour_step=None, nlevels=None, level_min=None,
 
     if sea_color is not None:
         cmap_sea = mcolors.ListedColormap([sea_color])
-        cmap_sea.set_under(color='w', alpha=0)
-        mask_sea = (z <= sea_level)*1
+        cmap_sea.set_under(color="w", alpha=0)
+        mask_sea = (z <= sea_level) * 1
         if mask_sea.any():
-            axe.imshow(mask_sea, extent=im_extent, cmap=cmap_sea,
-                       vmin=0.5, origin='lower', interpolation='none')
+            axe.imshow(
+                mask_sea,
+                extent=im_extent,
+                cmap=cmap_sea,
+                vmin=0.5,
+                origin="lower",
+                interpolation="none",
+            )
