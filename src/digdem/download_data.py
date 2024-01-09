1e19e4a380f996ca42de92fb9ab8312b52ecd16f
Peruzzetto  Marc <m.peruzzetto@brgm.fr>
2024-01-09 09:07:02 +0100

    feat(data): add function to dowload sample data
    Add function to download sample data for the rainier example

---
+++ src/digdem/download_data.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 13 10:39:05 2023
+
+@author: peruzzetto
+"""
+
+import requests
+
+import os
+
+
+def import_rainier_data(folder_out=None):
+    if folder_out is None:
+        folder_out = "."
+
+    file_save = os.path.join(folder_out, "raster_rainier.tif")
+    url_base = (
+        "https://github.com/marcperuz/digdem/raw/"
+        + "master/data/rainier_example/"
+    )
+    r = requests.get(url_base + "raster_rainier.tif")
+    with open(file_save, "wb") as f:
+        for morceau in r.iter_content(chunk_size=128):
+            f.write(morceau)
+
+    for fmt in ["shp", "dbf", "shx", "prj"]:
+        for file in ["sections", "scar"]:
+            file_save = os.path.join(folder_out, "{}.{}".format(file, fmt))
+            r = requests.get(url_base + "{}.{}".format(file, fmt))
+            with open(file_save, "wb") as f:
+                f.write(r.content)
