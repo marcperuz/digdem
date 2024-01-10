628bc727962a3126af5ea75bf288eee08be1ee03
Peruzzetto  Marc <m.peruzzetto@brgm.fr>
2024-01-10 17:02:01 +0100

    refactor(tests): add instance of SurfMod as pytest fixture
    Add a synthetic example of SurfMod instance as fixture in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,10 +10,29 @@ import pytest
 import numpy as np
 
 import digdem.section
+import digdem.surfmod
 
 
 @pytest.fixture
-def synthetic_case():
+def synthetic_profiles():
+    # Longitudinal cross-section
+    point1 = (0.1, 0.0)
+    point2 = (1.2, 0.0)
+    sec_long = digdem.section.Section(
+        [point1, point2], "A-B", normalized=False, dinterp=0.05
+    )
+
+    # Transversal cross-section
+    point1 = (0.6, -0.3)
+    point2 = (0.6, 0.3)
+    sec_trans = digdem.section.Section(
+        [point1, point2], "C-D", normalized=False, dinterp=0.05
+    )
+    return dict(sec_long=sec_long, sec_trans=sec_trans)
+
+
+@pytest.fixture
+def synthetic_example(synthetic_profiles):
     nx = 301
     ny = 201
     x = np.linspace(0, 1.5, 301)
@@ -36,24 +55,18 @@ def synthetic_case():
     ytest = y[ny // 2]
     ztest = zold[nx // 2, ny // 2]
 
-    return dict(
-        x=x, y=y, zold=zold, mask=mask, xtest=xtest, ytest=ytest, ztest=ztest
+    new_dem = digdem.surfmod.SurfMod(
+        zold,
+        mask,
+        x,
+        y,
+        sections=[
+            synthetic_profiles["sec_long"],
+            synthetic_profiles["sec_trans"],
+        ],
+        dinterp=0.05,
+        smooth=0,
+        sparse_interp=1,
     )
 
-
-@pytest.fixture
-def synthetic_profiles():
-    # Longitudinal cross-section
-    point1 = (0.1, 0.0)
-    point2 = (1.2, 0.0)
-    sec_long = digdem.section.Section(
-        [point1, point2], "A-B", normalized=False, dinterp=0.05
-    )
-
-    # Transversal cross-section
-    point1 = (0.6, -0.3)
-    point2 = (0.6, 0.3)
-    sec_trans = digdem.section.Section(
-        [point1, point2], "C-D", normalized=False, dinterp=0.05
-    )
-    return dict(sec_long=sec_long, sec_trans=sec_trans)
+    return dict(new_dem=new_dem, xtest=xtest, ytest=ytest, ztest=ztest)
--- tests/test_surfmod.py
+++ tests/test_surfmod.py
@@ -22,54 +22,29 @@ def test_Section_creation():
     assert np.abs(sec_long.line.length - 1.1) < 1e-6
 
 
-def test_SurfMod_creation(synthetic_case, synthetic_profiles):
-    new_dem = digdem.surfmod.SurfMod(
-        synthetic_case["zold"],
-        synthetic_case["mask"],
-        xaxis=synthetic_case["x"],
-        yaxis=synthetic_case["y"],
-        sections=[
-            synthetic_profiles["sec_long"],
-            synthetic_profiles["sec_trans"],
-        ],
-        dinterp=0.05,
-        smooth=0,
-        sparse_interp=1,
-    )
-    ztest = new_dem.fz_old(
-        synthetic_case["xtest"], synthetic_case["ytest"], grid=False
+def test_SurfMod_creation(synthetic_example):
+    ztest = synthetic_example["new_dem"].fz_old(
+        synthetic_example["xtest"], synthetic_example["ytest"], grid=False
     )
     # print(ztest, synthetic_case["ztest"])
     # assert ztest == synthetic_case["ztest"]
-    assert np.abs(ztest - synthetic_case["ztest"]) < 1e-6
+    assert np.abs(ztest - synthetic_example["ztest"]) < 1e-6
     # assert ztest == synthetic_case["ztest"]
 
 
-def test_SurfMod_interpolation(synthetic_case, synthetic_profiles):
-    new_dem = digdem.surfmod.SurfMod(
-        synthetic_case["zold"],
-        synthetic_case["mask"],
-        xaxis=synthetic_case["x"],
-        yaxis=synthetic_case["y"],
-        sections=[
-            synthetic_profiles["sec_long"],
-            synthetic_profiles["sec_trans"],
-        ],
-        dinterp=0.05,
-        smooth=0,
-        sparse_interp=1,
-    )
-
+def test_SurfMod_interpolation(synthetic_example, synthetic_profiles):
     pt = synthetic_profiles["sec_long"].line.intersection(
         synthetic_profiles["sec_trans"].line
     )
     xint = pt.xy[0][0]
     yint = pt.xy[1][0]
-    zint_old = new_dem.fz_old(xint, yint, grid=False)
+    zint_old = synthetic_example["new_dem"].fz_old(xint, yint, grid=False)
 
-    new_dem.set_intersections(["A-B"], ["C-D"], [zint_old - 0.2])
-    new_dem.update_interpolation()
+    synthetic_example["new_dem"].set_intersections(
+        ["A-B"], ["C-D"], [zint_old - 0.2]
+    )
+    synthetic_example["new_dem"].update_interpolation()
 
-    znew = new_dem.fz_new(xint, yint)
+    znew = synthetic_example["new_dem"].fz_new(xint, yint)
 
     assert np.abs(zint_old - 0.2 - znew) < 1e-4
